48b8fba7c117451004d6b788ee22f2d6384d59a3
Thiago Leite <thiagols@uol.com.br>
2023-06-05 00:22:41 -0300

    Setup project

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/Screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/Screens/Login/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+function Login() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Tela Inicial</Text>
+        </View>
+    );
+}
+
+export default Login;
